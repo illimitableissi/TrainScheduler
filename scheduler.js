c510febf5c3ce6825c75df1556939c4977e9788d
SOVEREIGN <isimemeni@gmail.com>
2019-10-06 18:10:46 -0400

    added firebase parameters

---
+++ scheduler.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCbzNgiavTBuK6rSpKmonHx9QHAwHyhtGI",
+    authDomain: "trainscheduler-e6f07.firebaseapp.com",
+    databaseURL: "https://trainscheduler-e6f07.firebaseio.com",
+    projectId: "trainscheduler-e6f07",
+    storageBucket: "",
+    messagingSenderId: "890525875713",
+    appId: "1:890525875713:web:5c9f5b8f24ea11371d4a00",
+    measurementId: "G-ELBB56GBBH"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
