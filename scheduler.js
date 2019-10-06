ce8012036d76c7f89e5188e7fbcde8ec24b9c55f
SOVEREIGN <isimemeni@gmail.com>
2019-10-06 19:04:14 -0400

    added js click function

--- scheduler.js
+++ scheduler.js
@@ -1,3 +1,4 @@
+// Firebase parameters
 var firebaseConfig = {
     apiKey: "AIzaSyCbzNgiavTBuK6rSpKmonHx9QHAwHyhtGI",
     authDomain: "trainscheduler-e6f07.firebaseapp.com",
@@ -8,6 +9,57 @@ var firebaseConfig = {
     appId: "1:890525875713:web:5c9f5b8f24ea11371d4a00",
     measurementId: "G-ELBB56GBBH"
   };
-  
+
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+
+// Click function on submit button click
+  $("submit-button").on("click", function(event) {
+    event.preventDefault();
+
+var trainName = $("train-name").val().trim();
+var trainDestination = $("train-destination").val().trim();
+var trainTime = $("train-time").val().trim();
+var trainFrequency = $("train-frequency").val().trim();
+
+
+var newTrain = {
+    name: trainName,
+    destination: trainDestination,
+    time: trainTime,
+    frequency: trainFrequency
+};
+
+  // Uploads employee data to the database
+  database.ref().push(newTrain);
+
+  trainName.val("");
+  trainDestination.val("");
+  trainTime.val("");
+  trainFrequency.val("");
+
+  });
+
+  database.ref().on("child_added", function(snapShot) {
+    console.log(snapShot.val());
+
+    var trainName = snapShot.val().name;
+    var trainDestination = snapShot.val().destination;
+    var trainTime = snapShot.val().time;
+    var trainFrequency = snapShot.val().frequency;
+
+  // Create the new row
+  var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(trainDestination),
+    $("<td>").text(trainTime),
+    $("<td>").text(trainFrequency),
+  );
+
+  // Append the new row to the table
+  $("#train-table > tbody").append(newRow);
+
+  });
